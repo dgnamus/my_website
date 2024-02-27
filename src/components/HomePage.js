f8bd7fc924866a31ecdde00d39d80f0154216365
David Gnamuš <38346533+dgnamus@users.noreply.github.com>
2024-02-27 18:29:27 +0100

    initial website

---
+++ src/components/HomePage.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+const HomePage = () => {
+  return (
+    <div>
+      <header>
+        <h1>My Simple React Home Page</h1>
+      </header>
+      <main>
+        <p>
+          Welcome to my simple React home page! This is a basic example of a
+          React project.
+        </p>
+        <img
+          src="https://s7g10.scene7.com/is/image/stena/20190129_gdansk-long-market:16-9?ts=1688733284120&dpr=off"
+          alt="Placeholder"
+        />
+      </main>
+    </div>
+  );
+};
+
+export default HomePage;
